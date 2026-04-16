c1e6e8cc18c5e55e5030cea9dd870ad63504d498
21140780 <l21140780@queretaro.tecnm.mx>
2026-04-16 17:54:44 -0600

    cambio de usuario

--- Ingenieria_de_Software/Unidad_3/backend-docly/db.js
+++ Ingenieria_de_Software/Unidad_3/backend-docly/db.js
@@ -7,7 +7,7 @@ oracledb.outFormat = oracledb.OUT_FORMAT_OBJECT;
 async function getConnection() {
     try {
         const connection = await oracledb.getConnection({
-            user: "backenduser",
+            user: "ADMIN",
             password: "zorroITQ2604",
             connectString: "(description= (retry_count=20)(retry_delay=3)(address=(protocol=tcps)(port=1521)(host=adb.mx-queretaro-1.oraclecloud.com))(connect_data=(service_name=g559b20f1aaca36_s52etnzap8x1a69t_low.adb.oraclecloud.com))(security=(ssl_server_dn_match=yes)))"
             //configDir: walletPath,

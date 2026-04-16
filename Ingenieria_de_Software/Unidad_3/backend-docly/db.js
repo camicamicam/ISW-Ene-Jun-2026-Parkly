7d367cf9a7cd5cc2f66e35e1068e1144011631c6
21140780 <l21140780@queretaro.tecnm.mx>
2026-04-16 10:43:39 -0600

    Conexión directa TLS a Oracle usando _low *sin wallet

--- Ingenieria_de_Software/Unidad_3/backend-docly/db.js
+++ Ingenieria_de_Software/Unidad_3/backend-docly/db.js
@@ -1,17 +1,15 @@
 const oracledb = require('oracledb');
-const path = require('path');
+//const path = require('path');
 
 oracledb.outFormat = oracledb.OUT_FORMAT_OBJECT;
-const walletPath = path.join(__dirname, 'wallet');
+//const walletPath = path.join(__dirname, 'wallet');
 
 async function getConnection() {
     try {
         const connection = await oracledb.getConnection({
             user: "backenduser",
             password: "zorroITQ2604",
-            connectString: "s52etnzap8x1a69t_high",
-            configDir: walletPath,
-            walletPassword: "zorroITQ2604"
+            connectString: "(description= (retry_count=20)(retry_delay=3)(address=(protocol=tcps)(port=1522)(host=adb.mx-queretaro-1.oraclecloud.com))(connect_data=(service_name=g559b20f1aaca36_s52etnzap8x1a69t_low.adb.oraclecloud.com))(security=(ssl_server_dn_match=yes)))"
         });
         return connection;
     } catch (err) {

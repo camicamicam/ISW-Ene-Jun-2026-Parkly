f781c2a58c48173b9582f6e526d0c1d52e6d82d6
21140780 <l21140780@queretaro.tecnm.mx>
2026-04-16 11:58:06 -0600

    password

--- Ingenieria_de_Software/Unidad_3/backend-docly/db.js
+++ Ingenieria_de_Software/Unidad_3/backend-docly/db.js
@@ -9,7 +9,8 @@ async function getConnection() {
         const connection = await oracledb.getConnection({
             user: "backenduser",
             password: "zorroITQ2604",
-            connectString: "(description= (retry_count=20)(retry_delay=3)(address=(protocol=tcps)(port=1522)(host=adb.mx-queretaro-1.oraclecloud.com))(connect_data=(service_name=g559b20f1aaca36_s52etnzap8x1a69t_low.adb.oraclecloud.com))(security=(ssl_server_dn_match=yes)))"
+            connectString: "(description= (retry_count=20)(retry_delay=3)(address=(protocol=tcps)(port=1522)(host=adb.mx-queretaro-1.oraclecloud.com))(connect_data=(service_name=g559b20f1aaca36_s52etnzap8x1a69t_low.adb.oraclecloud.com))(security=(ssl_server_dn_match=yes)))",
+            password: "zorroITQ2604"
         });
         return connection;
     } catch (err) {

f7675b97641ddb7dff77af997246566c8e1e2745
21140780 <l21140780@queretaro.tecnm.mx>
2026-04-16 18:31:40 -0600

    authModel general y token

---
+++ Ingenieria_de_Software/Unidad_3/backend-docly/models/authModel.js
@@ -0,0 +1,29 @@
+const db = require('../db');
+
+async function verificarCredenciales(password_acceso) {
+    let connection;
+    try {
+        connection = await db.getConnection();
+
+        const query = `
+            SELECT * FROM v_usuarios_roles 
+            WHERE password_acceso = :pass
+        `;
+
+        const result = await connection.execute(query, {
+            pass: password_acceso
+        });
+
+        return result.rows; 
+
+    } catch (error) {
+        console.error("Error en el modelo de auth:", error);
+        throw error;
+    } finally {
+        if (connection) {
+            try { await connection.close(); } catch (e) { console.error(e); }
+        }
+    }
+}
+
+module.exports = { verificarCredenciales };
